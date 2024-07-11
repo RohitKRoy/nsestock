e1feb26ccd3218395413bbc7b7c6299ad048e12d
RohitKumarRoy <ask2rohit@outlook.com>
2024-07-11 13:37:17 +0530

    initial commit

---
+++ src/main/java/nse/all/nsestock/App.java
@@ -0,0 +1,13 @@
+package nse.all.nsestock;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
